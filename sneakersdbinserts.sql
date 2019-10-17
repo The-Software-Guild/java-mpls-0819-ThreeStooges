e084c8b7b2d8454fe8c2018d0f484b853b8c031e
mikelee015 <mikelee015@gmail.com>
2019-10-17 12:47:04 -0500

    -adding sneaker db and inserts. removed initial db and added to new sneakerdb file.

---
+++ sneakersdbinserts.sql
@@ -0,0 +1,66 @@
+use SneakersDB;
+
+insert into brands (name) values 
+('nike'),
+('adidas'),
+('new balance'),
+('vans'),
+('air jordan');
+
+insert into primarycolors (name) values 
+('black'),
+('blue'),
+('white'),
+('gray'),
+('red'),
+('pink');
+
+insert into secondarycolors (name) values 
+('black'),
+('blue'),
+('white'),
+('gray'),
+('red'),
+('pink');
+
+insert into sizes (shoesize) values
+(1), (2), (3), (4), (5),
+(6), (7), (8), (9), (10),
+(11), (12), (13), (14), (15);
+
+insert into models (name, brandid, releaseyear, primarycolorid, secondarycolorid, msrp_price) values
+('jordan 1', 5, 1991, 5, 1, 150.00),
+('KD1', 1, 2012, 2, 3, 175.00),
+('yeezy', 2, 2015, 3, 4, 220.00);
+
+insert into types (name) values
+('infant'),
+('toddler'),
+('women'),
+('men');
+
+insert into models_types (modelid, typeid) values
+(1,4), (1,3), (2,4), (3,1), (3,2), (3,3), (3,4);
+
+insert into status (name) values
+('active'), ('inactive'), ('sold');
+
+insert into shoeconditions (name) values
+('new'), ('used');
+
+insert into listings 
+(statusid, modelid, shoeconditionid, shoesizeid, description, 
+buynowprice, minstartingprice, listdate, enddate, sellerid, photo)
+values
+(1, 1, 1, 10, 'original AJ1 in box', 200.00, 150.00, '2019-10-01', '2019-10-30', 3, 'xyz.png'),
+(3, 3, 2, 15, 'turtledoves slightly used', 220.00, 190.00,  '2019-09-15', '2019-10-2', 3, 'abc.jpg');
+
+insert into purchases (listingid, saleprice, sellerid, buyerid, datesold)
+values
+(2, 265.00, 3, 2, '2019-10-07');
+
+insert into bids (listingid, bidprice, date) values
+(1, 205.00, '2019-10-02'),
+(1, 210.00, '2019-10-25'),
+(2, 220.00, '2019-10-01');
+
---
+++ sneakersdbschema.sql
@@ -0,0 +1,134 @@
+drop database if exists SneakersDB;
+
+create database if not exists SneakersDB;
+
+use SneakersDB;
+
+
+create table `users`(
+`id` int primary key auto_increment,
+`username` varchar(30) not null unique,
+`password` varchar(100) not null,
+`enabled` boolean not null);
+
+create table `roles`(
+`id` int primary key auto_increment,
+`role` varchar(30) not null
+);
+
+create table `users_roles`(
+`user_id` int not null,
+`role_id` int not null,
+primary key(`user_id`,`role_id`),
+foreign key (`user_id`) references `users`(`id`),
+foreign key (`role_id`) references `roles`(`id`));
+
+insert into `users`(`id`,`username`,`password`,`enabled`)
+    values(1,"admin", "$2a$10$DlLWMpK5t5o/UxLj5tOJfO9UgtY78M23DYjUidZ71emys4r7kycny", true),
+        (2,"buyer","$2a$10$DlLWMpK5t5o/UxLj5tOJfO9UgtY78M23DYjUidZ71emys4r7kycny",true),
+        (3,"seller", "$2a$10$DlLWMpK5t5o/UxLj5tOJfO9UgtY78M23DYjUidZ71emys4r7kycny", true);
+
+insert into `roles`(`id`,`role`)
+    values(1,"ROLE_ADMIN"), (2,"ROLE_BUYER"), (3,"ROLE_SELLER");
+    
+insert into `users_roles`(`user_id`,`role_id`)
+    values(1,1),(1,2),(1,3),(2,2),(3,2),(3,3);
+
+
+create table brands (
+	id					int					primary key auto_increment,
+	name 			varchar(100)			not null
+);
+
+create table primarycolors (
+	id 					int					primary key auto_increment,
+	name				varchar(50)			not null
+);
+
+create table secondarycolors (
+	id 					int					primary key auto_increment,
+	name				varchar(50)			not null
+);
+
+create table sizes (
+	shoesize			int					primary key
+);
+
+create table models (
+	id					int					primary key auto_increment,
+	name				varchar(100)		not null,
+	brandid				int					not null,
+	releaseyear			int					not null,
+	primarycolorid		int					not null,
+    secondarycolorid	int					null,
+	msrp_price			decimal(8,2)		not null,
+		foreign key fk_brands_models (brandid) references brands (id),
+		foreign key fk_primarycolors_models (primarycolorid) references primarycolors (id),
+        foreign key fk_secondarycolors_models (secondarycolorid) references secondarycolors (id)
+);
+
+create table types (
+	id					int					primary key auto_increment,
+    name				varchar(50)			not null
+    );
+    
+create table models_types (
+	modelid				int					not null,
+    typeid				int					not null,
+		foreign key fk_models_models_types (modelid) references models (id),
+        foreign key fk_types_models_types (typeid) references types (id)
+);
+    
+create table status (
+	id					int					primary key auto_increment,
+    name				varchar(50)			not null
+);
+
+create table shoeconditions (
+	id					int					primary key auto_increment,
+    name				varchar(50)			not null
+);    
+    
+
+create table listings (
+	id					int					primary key auto_increment,
+	statusid			int					not null,
+	modelid				int					not null,
+	shoeconditionid		int					not null,
+	shoesizeid			int					not null,			
+	description			varchar(1000)		not null,
+    buynowprice			decimal(8,2)		not null,
+    minstartingprice	decimal(8,2)		not null,
+	listdate			date				not null,
+	enddate				date				null, -- this is the listing end date (i.e. the deadline for bids to be entered)
+	sellerid			int					not null,
+	photo				varchar(1000)		null,
+		foreign key fk_status_listings (statusid) references status (id),
+		foreign key fk_models_listings (modelid) references models (id),
+        foreign key fk_shoeconditions_listings (shoeconditionid) references shoeconditions (id),
+        foreign key fk_sizes_listings (shoesizeid) references sizes (shoesize),
+		foreign key fk_users_listings (sellerid) references users (id)
+);
+
+
+create table purchases (
+	id					int					primary key auto_increment,
+    listingid			int					not null,
+    saleprice			decimal(8,2)		not null,
+    sellerid			int					not null,
+    buyerid				int					not null,
+    datesold			date				not null,
+		foreign key fk_listings_purchases (listingid) references listings (id),
+        foreign key fk_suser_purchases (sellerid) references users (id),
+        foreign key fk_buser_purchases (buyerid) references users (id)
+);
+
+create table bids (
+	id					int					primary key auto_increment,
+	listingid			int					not null,
+    bidprice			decimal(8,2)		not null,
+    date				date				not null,
+		foreign key fk_listings_bids (listingid) references listings (id)
+);
+
+
